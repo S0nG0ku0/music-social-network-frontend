2a7319f56f9d35dfd72f3197cd0c08da28640c70
unknown <haidaomar663@gmail.com>
2023-01-02 14:34:04 +0100

    [edit: EditProfile => Form]

--- src/views/account/EditProfile.vue
+++ src/views/account/EditProfile.vue
@@ -34,14 +34,41 @@
         />
       </div>
     </div>
+    <div class="flex flex-wrap mt-4 mb-6">
+      <div class="w-full md:w-1/2 px-3">
+        <DisplayCropperButton
+          label="Profile Image"
+          btnText="Update Profile Image"
+        />
+      </div>
+    </div>
+    <div class="flex flex-wrap mt-4 mb-6">
+      <div class="w-full px-3">
+        <TextArea
+          label="Description"
+          placeholder="Please entre some information"
+          v-model="description"
+          error="This is a test error"
+        />
+      </div>
+    </div>
+    <div class="flex flex-wrap mt-8 mb-6">
+      <div class="w-full px-3">
+        <SubmitFormButton btnText="Update Profile" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup>
 import { ref } from "vue";
 import TextInput from "@/components/global/TextInput.vue";
+import DisplayCropperButton from "@/components/global/DisplayCropperButton.vue";
+import TextArea from "@/components/global/TextArea.vue";
+import SubmitFormButton from "@/components/global/SubmitFormButton.vue";
 
 let firstName = ref(null);
 let lastName = ref(null);
 let location = ref(null);
+let desciption = ref(null);
 </script>
